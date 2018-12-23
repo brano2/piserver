4d5c5fa78d9fb8d75d779ee1aa32ea705377237e
Branislav Pilnan <B.Pilnan@gmail.com>
2018-12-23 17:17:35 +0000

    Implement the camera page

--- app/routes.py
+++ app/routes.py
@@ -2,6 +2,7 @@ from flask import render_template
 
 from app import app
 from my_hw.thermometer import get_temperature
+from my_hw.camera import capture
 
 
 @app.route('/')
@@ -12,3 +13,8 @@ def index():
 @app.route('/temperature')
 def temperature():
     return '{} °C'.format(get_temperature())
+
+@app.route('/camera')
+def camera():
+    capture('static')
+    return render_template('camera.html')
